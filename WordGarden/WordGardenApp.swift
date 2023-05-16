d7c36d96ddf262517fff3fd86bc77ba9e06a90df
Jake Tamayo <hellojtmatthew@icloud.com>
2023-05-16 10:24:39 -0500

    Initial Commit

---
+++ WordGarden/WordGardenApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordGardenApp.swift
+//  WordGarden
+//
+//  Created by Jacob Tamayo on 5/16/23.
+//
+
+import SwiftUI
+
+@main
+struct WordGardenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
